7f0c73dad3c214555594fbb7209ddba72f216d8c
Anthony Phelps <anthonypphelps@gmail.com>
2012-10-10 15:05:56 +0000

    Started cutting tiles

--- main.cpp
+++ main.cpp
@@ -19,14 +19,27 @@ int main(int argc, char *argv[])
     int screenWidth = desktop->width();
     //int screenHeight = desktop->height();
 
-    QLabel *imageLabel = new QLabel(&mainWindow);
+    //QLabel *imageLabel = new QLabel(&mainWindow);
     QImage elephant(":/elephant.gif");
 
     elephant = elephant.scaledToWidth(screenWidth);
+    int eWidth = elephant.width();
+    int eHeight = elephant.height();
 
-    imageLabel->setPixmap(QPixmap::fromImage(elephant));
-    imageLabel->resize(elephant.width(), elephant.height());
-    imageLabel->show();
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            QLabel *imageLabel = new QLabel(&mainWindow);
+            imageLabel->setPixmap(QPixmap::fromImage(elephant.copy(i*(eWidth/5), j*(eHeight/5), (i+1)*(eWidth/5), (j+1)*(eHeight/5))));
+            imageLabel->resize(eWidth, eHeight);
+            imageLabel->show();
+        }
+    }
+
+    //imageLabel->setPixmap(QPixmap::fromImage(elephant));
+   // imageLabel->resize(elephant.width(), elephant.height());
+   // imageLabel->show();
 
     return app.exec();
 }
