d021af5f68adaa582ad3936a09fc9b53fa7a2e6d
Matt Dumford <mdumford99@gmail.com>
2012-10-10 16:23:13 -0500

    woops forgot to save comments

--- main.cpp
+++ main.cpp
@@ -22,10 +22,12 @@ int main(int argc, char *argv[])
     mainWindow.resize(screenWidth, screenHeight);
     mainWindow.showExpanded();
 
+    //set up scene and view
     QGraphicsScene *gScene = new QGraphicsScene(&mainWindow);
     QGraphicsView *gView = new QGraphicsView(gScene);
     gScene->setBackgroundBrush(Qt::black);
 
+    //import and resize image
     QImage elephant(":/elephant.gif");
     elephant = elephant.scaledToWidth(screenWidth);
     int eWidth = elephant.width();
@@ -33,16 +35,18 @@ int main(int argc, char *argv[])
 
     //gScene->addPixmap(QPixmap::fromImage(elephant));
 
+    //cut image into tiles and position them
     for(int i = 0; i < 5; i++)
     {
         for(int j = 0; j < 5; j++)
         {
-
            QGraphicsPixmapItem *item = gScene->addPixmap(QPixmap::fromImage(elephant.copy(i*(eWidth/5), j*(eHeight/5), eWidth/5, eHeight/5)));
            item->setPos(eWidth/5 *i, eHeight/5 *j);
            item->setScale(0.99);
         }
     }
+
+
     gView->show();
     return app.exec();
 }
